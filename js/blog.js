4d51dab2c41634b5e98f7209662407b795e10609
HardikBansal206 <126601470+HardikBansal206@users.noreply.github.com>
2023-07-30 02:22:39 +0530

    blog page almost done

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,52 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
     img = document.getElementById("img1");
   
-    if (dots.style.display === "none") {
+    if (dots.style.display == "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+      img.style.transform = "scale(1)";
+			img.style.transition = "transform 0.25s ease";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+      img.style.transform = "scale(1.5)";
+			img.style.transition = "transform 0.25s ease";
+    }
+  }
+
+  function myFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+    img = document.getElementById("img2");
+  
+    if (dots.style.display == "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+      img.style.transform = "scale(1)";
+			img.style.transition = "transform 0.25s ease";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+      img.style.transform = "scale(1.5)";
+			img.style.transition = "transform 0.25s ease";
+    }
+  }
+
+  function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+    img = document.getElementById("img3");
+  
+    if (dots.style.display == "none") {
       dots.style.display = "inline";
       btnText.innerHTML = "Read more";
       moreText.style.display = "none";
