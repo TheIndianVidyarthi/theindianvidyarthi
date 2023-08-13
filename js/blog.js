d40e6c0968d6fa91a2df1eea618855d3066d98d7
tarush10000 <tarushagarwal2003@gmail.com>
2023-08-13 22:25:18 +0530

    yess

--- js/blog.js
+++ js/blog.js
@@ -1,62 +1,20 @@
-function myFunction1() {
-    var dots = document.getElementById("dots1");
-    var moreText = document.getElementById("more1");
-    var btnText = document.getElementById("myBtn1");
-    img = document.getElementById("img1");
-  
-    if (dots.style.display == "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-      img.style.transform = "scale(1)";
-			img.style.transition = "transform 0.25s ease";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-      img.style.transform = "scale(1.5)";
-			img.style.transition = "transform 0.25s ease";
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    autoplay:true,   
+    smartSpeed: 3000, 
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
     }
-  }
-
-  function myFunction2() {
-    var dots = document.getElementById("dots2");
-    var moreText = document.getElementById("more2");
-    var btnText = document.getElementById("myBtn2");
-    img = document.getElementById("img2");
-  
-    if (dots.style.display == "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-      img.style.transform = "scale(1)";
-			img.style.transition = "transform 0.25s ease";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-      img.style.transform = "scale(1.5)";
-			img.style.transition = "transform 0.25s ease";
-    }
-  }
-
-  function myFunction3() {
-    var dots = document.getElementById("dots3");
-    var moreText = document.getElementById("more3");
-    var btnText = document.getElementById("myBtn3");
-    img = document.getElementById("img3");
-  
-    if (dots.style.display == "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-      img.style.transform = "scale(1)";
-			img.style.transition = "transform 0.25s ease";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-      img.style.transform = "scale(1.5)";
-			img.style.transition = "transform 0.25s ease";
-    }
-  }
+})
