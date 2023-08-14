71b3bd6b7221dc68cf8c25f517966e0d1669c033
hardiksingla <07hardiksingla@gmail.com>
2023-08-14 18:49:10 +0530

    removed scroll js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,2 @@
-// for the about page
-function scroll() {
-  const htmlElement = document.documentElement
-  var overlay = document.getElementById("About-content");
-  var videoBackground = document.getElementById("About-bg");
-  var scrollPosition = window.scrollY;
-
-  // var percentageOfScreenScrolled = htmlElement.scrollTop/htmlElement.clientHeight;
-  // console.log(percentageOfScreenScrolled)
-  
-  
-  let opacityValue = scrollPosition / 75 - 9; // Adjust the divisor to control the animation speed and adjust the subtracrted value to control when the opacity changes
-  overlay.style.transition = "opacity 0.1s"; // Set the transition duration to 0.1 seconds (adjust as needed)
-  overlay.style.opacity = opacityValue;
-
-
-}
-
-window.addEventListener("scroll", scroll);
-
-
+var overlay = document.getElementById("About-content");
+overlay.style.opacity = 1;
