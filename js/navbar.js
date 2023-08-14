89c9469c04f6ff5a762b5286a3c6a3354660767d
hardiksingla <07hardiksingla@gmail.com>
2023-08-14 12:26:33 +0530

    minor bug fix

--- js/navbar.js
+++ js/navbar.js
@@ -2,14 +2,15 @@ const icon = document.getElementById("hamburger")
 const menu = document.getElementById("menu")
 const nav = document.getElementById("nav")
 const body = document.querySelector("body")
-console.log(body)
-console.log(icon)
-icon.addEventListener("click",()=>{
+
+function menuToggle() {
   menu.classList.toggle('show')
   nav.classList.toggle('show')
   icon.classList.toggle("change");
   body.classList.toggle("show");
-})
+}
+
+icon.addEventListener("click", menuToggle)
 
 window.onresize = function() {
   clientWidth = document.documentElement.clientWidth
@@ -18,3 +19,8 @@ window.onresize = function() {
 var clientWidth = document.documentElement.clientWidth
 
 nav.style.width = clientWidth + "px"
+
+// nav.style.display = "none"
+// window.addEventListener("scroll", ()=>{
+//   nav.style.display = "flex"
+// });
